87854386f376042f56dd8033a3609785ee781266
Sammy <bavesh2006kumar@gmail.com>
2026-06-07 19:56:01 +0530

    added frontend

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/service')
+def service():
+    return render_template('services.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
